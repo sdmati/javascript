2084dbf6a8793a6010a77a1ba35a7a781d80747e
sdmati <83706221+sdmati@users.noreply.github.com>
2022-10-02 13:53:56 +0200

    Add files via upload

---
+++ kalkulator/script.js
@@ -0,0 +1,43 @@
+function suma(){
+
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+    
+    alert(a+b);
+}
+
+
+function roznica(){
+
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+    alert(a-b);
+}
+
+
+function iloczyn(){
+
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+    alert(a*b);
+}
+
+
+function iloraz(){
+
+    var a = document.getElementById("a").value;
+    var b = document.getElementById("b").value;
+
+    a = parseFloat(a);
+    b = parseFloat(b);
+    alert(a/b);
+}
