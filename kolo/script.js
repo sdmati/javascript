b1f555a08d768b3dee909887f4f558064866b435
sdmati <83706221+sdmati@users.noreply.github.com>
2022-12-08 22:22:58 +0100

    Add files via upload

---
+++ kolo/script.js
@@ -0,0 +1,8 @@
+function poletrojkata(){
+
+    var a = document.getElementById("a").value
+    var h = document.getElementById("h").value
+
+    var wynik = document.getElementById("wynik")
+    wynik.value = a * h / 2
+}
