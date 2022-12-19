bb71b5f0c5c15241c8910b1a8e767805304ed5aa
sdmati <83706221+sdmati@users.noreply.github.com>
2022-12-19 17:50:57 +0100

    Add files via upload

---
+++ switch/script.js
@@ -0,0 +1,16 @@
+var a = Text(prompt("siema"))
+
+switch(siema){
+
+    case 1:
+        document.write("hej")
+        break;
+
+    case 2:
+        document.write("siema")
+        break;
+
+    default:
+        document.write("pierdol sie")
+        break;
+}
